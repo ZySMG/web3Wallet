54aba634420216bc0ce8dacde01085cfb4a2c033
yu <test@gmail.com>
2025-10-26 15:32:04 +0800

    Initial Commit

---
+++ trust_wallet2/trust_wallet2App.swift
@@ -0,0 +1,17 @@
+//
+//  trust_wallet2App.swift
+//  trust_wallet2
+//
+//  Created by 张雨 on 2025/10/26.
+//
+
+import SwiftUI
+
+@main
+struct trust_wallet2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
